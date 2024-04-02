d89f22b3e661de12349d930dedfa3b206f6f9dcc
PieterNatanael <pieternatanael@icloud.com>
2024-04-02 20:08:49 +0800

    Initial Commit

---
+++ Sing Loop/Sing_LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sing_LoopApp.swift
+//  Sing Loop
+//
+//  Created by Pieter Yoshua Natanael on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Sing_LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
