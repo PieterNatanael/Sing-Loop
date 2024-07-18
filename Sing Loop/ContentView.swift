15a5a3c7b57f7090b8351959c04004cd37748e4c
PieterNatanael <pieternatanael@icloud.com>
2024-07-18 15:30:33 +0800

    Create README.md

--- Sing Loop/ContentView.swift
+++ Sing Loop/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             
+            
             VStack(spacing: 20) {
                 // App Title and Buttons
                 HStack {
